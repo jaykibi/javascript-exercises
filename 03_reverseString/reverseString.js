8877052fc5520ef039bcece9a574e1dcdfeaabf3
kibi <raquibbi@gmail.com>
2023-12-02 01:02:46 -0600

    finished assignment 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,24 @@
-const reverseString = function() {
+/*
+psuedo 
 
+1. grab string from user and store string into variable
+2. split string by chars into a list 
+3. initialize return string variable
+4. loop over split string list from the last element -> first 
+5. for each iteration add element to return string variable
+6. return return string variable
+
+*/
+
+const reverseString = function (inputString) {
+  let stringArray = inputString.split("");
+  let reverseArray = [];
+
+  for (let i = 0; i < inputString.length; i++) {
+    reverseArray.push(stringArray.pop());
+  }
+
+  return reverseArray.join("");
 };
 
 // Do not edit below this line
