f110cd20fd95b2c6909f9216b4866bb514c7d69e
kibi <raquibbi@gmail.com>
2023-12-03 00:21:25 -0600

    finished assignment 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,24 @@
-const convertToCelsius = function() {
+/*  
+Celsius to Fahrenheit: °F = °C × (9/5) + 32
+Fahrenheit to Celsius: °C = (°F - 32) × 5/9
+*/
+
+const convertToCelsius = function (temp) {
+  let returnTemp = ((temp - 32) * 5) / 9;
+
+  returnTemp = Math.round(returnTemp * 10) / 10;
+  return returnTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let returnTemp = temp * (9 / 5) + 32;
+
+  returnTemp = Math.round(returnTemp * 10) / 10;
+  return returnTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
